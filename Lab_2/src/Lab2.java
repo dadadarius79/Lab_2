c9e3c188ab49316c484e00abd3ead7d216fc3d7c
Cristurean Darius <cristureandarius0@gmail.com>
2019-02-27 10:35:45 -0800

    Lab1 HOmework

---
+++ Lab_2/src/Lab2.java
@@ -0,0 +1,18 @@
+public class Province {
+    private String name;                    // "British Columbia"
+    private String capital;                 // "Victoria"
+    private int populationInMillions;       // 4 or 38
+}
+
+public Province{
+        if(isValidPopulation(populationInMillions) && (isValidProvince(province) &&
+        (isValidCapitalCity(capital)){
+        this.populationInMillions = 0 and 38;
+        this.name = name;
+        this.capital = capital;
+        } else {
+        this.populationInMillions = 4;
+        this.name = "B.C";
+        this.capital = "Victoria"
+        }
+        }
